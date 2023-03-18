eebb797c72d4f04365ee010b228ea915aa1fa02a
VinGP <105109338+VinGP@users.noreply.github.com>
2023-03-18 09:03:50 +0300

    DOMAIN variable in .env

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
+. .env
 if ! [ -x "$(command -v docker-compose)" ]; then
   echo 'Error: docker-compose is not installed.' >&2
   exit 1
 fi
-
-domains=($DOMAIN $DOAMIN_WWW)
+domains=("$DOMAIN" "$DOMAIN_WWW")
 rsa_key_size=4096
 data_path="./certbot"
-email=$EMAIL # Adding a valid address is strongly recommended
+email="$EMAI" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
